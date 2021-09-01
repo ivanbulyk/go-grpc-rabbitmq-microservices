d755006286aff086117f4d57d8d44c680dad93e6
Ivan Bulyk <stackyogi@gmail.com>
2021-09-01 19:00:49 +0300

    add RabitMQ

--- go.mod
+++ go.mod
@@ -17,6 +17,7 @@ require (
 	github.com/jackc/pgproto3/v2 v2.1.1 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20200714003250-2b9c44734f2b // indirect
 	github.com/jackc/pgtype v1.8.1 // indirect
+	github.com/streadway/amqp v1.0.0 // indirect
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
 	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
--- usermgmt_user_cash/usermgmt_user_cash.go
+++ usermgmt_user_cash/usermgmt_user_cash.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"time"
 
 	pb "github.com/ivanbulyk/go-grpc-rabbitmq-microservices/usermgmt"
+	"github.com/streadway/amqp"
 	"google.golang.org/grpc"
 )
+
+type User struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+	ID   int    `json:"id"`
+}
 
 const (
 	address = "localhost: 50051"
@@ -26,17 +34,68 @@ func main() {
 	defer cancel()
 
 	var new_users = make(map[string]int32)
+
 	new_users["Alice"] = 43
 	new_users["Bob"] = 30
+
+	connRabbit, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+	defer connRabbit.Close()
+
+	fmt.Println("Successfully connected to  RabbitMq instance")
+
+	ch, err := connRabbit.Channel()
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"TestQueue",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Fatalf("Could not declare a queue: %v", err)
+	}
+	fmt.Println(q)
+
 	for name, age := range new_users {
 		r, err := c.CreateNewUser(ctx, &pb.NewUser{Name: name, Age: age})
 		if err != nil {
 			log.Fatalf("could not create user: %v", err)
 		}
+
 		log.Printf(`User Details:
 NAME: %s
 AGE: %d
 ID: %d`, r.GetName(), r.GetAge(), r.GetId())
+		userData := User{Name: r.GetName(), Age: int(r.GetAge()), ID: int(r.GetId())}
+		body, err := json.Marshal(userData)
+		if err != nil {
+			log.Printf("Error encoding JSON %v", err)
+		}
+
+		err = ch.Publish(
+			"",
+			"TestQueue",
+			false,
+			false,
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body:        body,
+			},
+		)
+		if err != nil {
+			log.Fatalf("Could not published message to a queue: %v", err)
+		}
+		fmt.Println("Succesfully Published Message to Queue")
 	}
 	params := &pb.GetUsersParams{}
 	r, err := c.GetUsers(ctx, params)
@@ -45,4 +104,5 @@ ID: %d`, r.GetName(), r.GetAge(), r.GetId())
 	}
 	log.Print("\nUSER LIST: \n")
 	fmt.Printf("r.GetUsers(): %v\n", r.GetUsers())
+
 }
